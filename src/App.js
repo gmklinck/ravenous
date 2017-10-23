25e546efa1cc4f5919c105707faa47a2c12f9d54
gmklinck <32417977+gmklinck@users.noreply.github.com>
2017-10-23 18:28:46 -0500

    pt 4 WIP
    working on figuring out errors

--- src/App.js
+++ src/App.js
@@ -3,24 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import BusinessList from '../src/components/BusinessList/BusinessList';
 import SearchBar from '../src/components/SearchBar/SearchBar';
+import Yelp from '../src/util/Yelp';
 
 const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90,
+
 }
 
-const businesses = [business, business, business, business, business, business];
-
 class App extends Component {
+  constuctor() {
+    this.state = {
+      businesses: []
+    }
+    this.searchYelp = this.searchYelp.bind(this);
+  }
   searchYelp(term, location, sortBy) {
-    console.log('Searching Yelp with ' + term + ', ' + location + ', and ' + sortBy);
+    Yelp.search(term, location, sortBy).then(businesses => {
+      setState({businessess: this.state.businesses})
+    })
   }
   render() {
     return (
---
+++ src/util/Yelp.js
@@ -0,0 +1,39 @@
+const clientId = 'eceJTfQmZj4Vk-zzSzh0KQ';
+const secret = 'mQ1g1hqTugBkFO1kUhKjrufOEVidVVLvZ34ULy3SqdbPvAXZnaOQppZZ8d5SBbm3';
+const accessToken
+
+const Yelp = {
+  getAccessToken() {
+    if(accessToken) {
+      return new Promise(resolve => resolve(accessToken));
+    }
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`,
+    {method: 'POST'}).then(response => {
+      return response.json()
+    }).then(jsonResponse => {
+      accessToken = jsonResponse.access_token;
+    });
+  },
+  search(term, location, sortBy) {
+    return Yelp.getAccessToken().then(function => {
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {headers: {Authorization: `Bearer ${accessToken}`}});
+    }).then(response => response.json()).then(jsonResponse => {
+      if(jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => {
+          id: business.id;
+          imageSrc: business.image_url;
+          name: business.name;
+          address: business.address1;
+          city: business.city;
+          state: business.state;
+          zipCode: business.zip_code;
+          category: business.title;
+          rating: business.rating;
+          reviewCount: business.review_count;
+        })
+      }
+    })
+  }
+};
+export default Yelp;
