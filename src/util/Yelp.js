2497827b70ccfadb58e6c1a43db042aa5ea084ac
gmklinck <32417977+gmklinck@users.noreply.github.com>
2017-10-24 11:31:05 -0500

    pt4 updated v3
    cors app returning error on my app and on codecademy site, everything compiling now but lets go button no longer returning cors error 503, keys in business no longer returning as undefined

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -7,7 +7,7 @@ const Yelp = {
     if(accessToken) {
       return new Promise(resolve => resolve(accessToken));
     }
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`,
+    return fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=' + clientId + '&client_secret=' + secret,
     {method: 'POST'}).then(response => {
       return response.json()
     }).then(jsonResponse => {
@@ -18,21 +18,23 @@ const Yelp = {
     return Yelp.getAccessToken().then(() => {
       return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
       {headers: {Authorization: `Bearer ${accessToken}`}});
-    }).then(response => response.json()).then(jsonResponse => {
+    }).then(response => {
+      return response.json()
+    }).then(jsonResponse => {
       if(jsonResponse.businesses) {
-        return jsonResponse.businesses.map(business => {
-          id: business.id;
-          imageSrc: business.image_url;
-          name: business.name;
-          address: business.address1;
-          city: business.city;
-          state: business.state;
-          zipCode: business.zip_code;
-          category: business.title;
-          rating: business.rating;
-          reviewCount: business.review_count;
-        })
-      }
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.address1,
+          city: business.city,
+          state: business.state,
+          zipCode: business.zip_code,
+          category: business.title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }))
+      };
     })
   }
 };
